402c1e0347c3a8d9f52988865266dac465e836c5
devi <133452064+infdevv@users.noreply.github.com>
2025-07-25 00:28:53 +0000

    v2 please vercel stop bitching

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,14 @@
 
+// atob is d
+
 self.__uv$config = {
     prefix: "/assets-images/",
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    	handler: "/uv.handler.js",
-	client: "/uv.client.js",
-	bundle: "/uv.bundle.js",
-	config: "/uv.config.js",
-	sw: "/uv.sw.js",
+    encodeUrl: url => Ultraviolet.codec.xor.encode((url)),
+    decodeUrl: url => Ultraviolet.codec.xor.decode((url)),
+    handler: "/uv/uv.handler.js",
+    client: "/uv/uv.client.js",
+    bare: "/bare/",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
     sw: "/uv/uv.sw.js",
   };
